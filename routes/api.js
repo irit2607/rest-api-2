d5d99347aa53cb85931c8d80f19de336b4731060
Irit Kushwaha <iritkushwaha2607@gmail.com>
2021-11-11 18:03:57 +0530

    delete request

--- routes/api.js
+++ routes/api.js
@@ -22,7 +22,10 @@ router.put('/ninjas/:id' ,function(req,res,next){
 
 // delete a ninja in the db
 router.delete('/ninjas/:id' ,function(req,res,next){
-    res.send({type:'DELETE'});
+    Ninja.findByIdAndRemove({ _id: req.params.id}).then(function(ninja){
+        res.send(ninja);
+    });
+   
 });
 
 module.exports = router;
